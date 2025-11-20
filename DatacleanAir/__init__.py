01cc9ac4a2e886ba3578cd47609abfbf3c8242d5
luisfmuniztorres <luisfmuniztorres@gmail.com>
2025-11-20 15:38:11 -0600

    Update __init__.py

--- DatacleanAir/__init__.py
+++ DatacleanAir/__init__.py
@@ -3,6 +3,8 @@
 from .filters import kalman_filter, savgol_filter_wrapper
 from .outliers import hampel_filter, iqr_filter, rate_of_change_filter
 from .qc import clean_series_pipeline
+from .utils import z_score_normalize
+
 
 __all__ = [
     "kalman_filter",
@@ -11,5 +13,7 @@ __all__ = [
     "iqr_filter",
     "rate_of_change_filter",
     "clean_series_pipeline",
+    "z_score_normalize",
 ]
+
 __version__ = "0.1.0"
